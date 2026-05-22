451586f6e181ed10635fc710df927a076e9ae82e
test-ko-bit <test.sql.0110@gmail.com>
2026-05-22 11:37:17 +0900

    Delete kadai_015 directory

--- kadai_015/CarExec_Chapter15.java
+++
@@ -1,7 +0,0 @@
-public class CarExec_Chapter15 {
-    public static void main(String[] args){
-        Car_Chapter15 car = new Car_Chapter15();
-        car.changeGear(3);
-        car.run();
-    }
-}
